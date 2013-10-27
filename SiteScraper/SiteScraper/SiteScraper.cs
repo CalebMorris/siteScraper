180dd81b40357f5af5c5a282c5d8b1a067da2c77
Caleb Morris <caleb.morris.g@gmail.com>
2013-10-27 11:47:40 -0700

    Completed base functionality.
    Readjusts the links in html files to be relative paths to the structure.

--- SiteScraper/SiteScraper/SiteScraper.cs
+++ SiteScraper/SiteScraper/SiteScraper.cs
@@ -20,6 +20,8 @@
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 using System;
 using System.Collections.Concurrent;
+using System.IO;
+using System.Linq;
 
 namespace SiteScraper
 {
@@ -27,28 +29,66 @@ namespace SiteScraper
 	{
 		public SiteScraper(string[] args)
 		{
-			m_usage = string.Format("Usage: {0} [url,]",System.AppDomain.CurrentDomain.FriendlyName);
-			if (args.Length == 0)
+			m_usage = string.Format("Usage: {0} [[url path],]",System.AppDomain.CurrentDomain.FriendlyName);
+			if (args.Length == 0 || args.Length % 2 == 1)
 			{
 				System.Console.Error.WriteLine(m_usage);
 				Environment.Exit(-1);
 			}
 			m_args = args;
-			m_urlQueue = new ConcurrentQueue<string>(args);
+			m_urlQueue = new ConcurrentQueue<Tuple<string,string>>();
+			for (int i = 0; i < args.Length; i+=2)
+				m_urlQueue.Enqueue(new Tuple<string,string>(args[i],args[i+1]));
 		}
 
 		public void Scrape()
 		{
-			string url;
+			System.Console.WriteLine("pwd:{0}", Directory.GetCurrentDirectory());
+			Tuple<string, string> urlPathTuple;
 			while (!m_urlQueue.IsEmpty)
 			{
-				m_urlQueue.TryDequeue(out url);
-				SiteScraperUtility.Scrape(url, "");
+				m_urlQueue.TryDequeue(out urlPathTuple);
+				if (!Directory.Exists(urlPathTuple.Item2))
+				{
+					System.Console.Error.WriteLine("The following path doesn't exist: {0}", urlPathTuple.Item2);
+					Environment.Exit(-1);
+				}
+				try
+				{
+					Uri uri = null;
+					if (!urlPathTuple.Item1.StartsWith("http"))
+						uri = new Uri(string.Format("http://{0}",urlPathTuple.Item1));
+					else
+						uri = new Uri(urlPathTuple.Item1);
+
+					if (uri.Scheme != Uri.UriSchemeHttp)
+						throw new UriFormatException("Scheme Not Supported: uri is not of the HTTP scheme.");
+
+					string siteDirectory = Path.Combine(urlPathTuple.Item2, uri.Host.Split('.').Reverse().Skip(1).First());
+					if (!SiteScraperUtility.FileOrDirectoryExists(siteDirectory))
+						Directory.CreateDirectory(siteDirectory);
+					else
+					{
+						int i = 2;
+						while (SiteScraperUtility.FileOrDirectoryExists(string.Format("{0}({1})", siteDirectory, i)))
+							i++;
+						siteDirectory = string.Format("{0}({1})", siteDirectory, i);
+						Directory.CreateDirectory(siteDirectory);
+					}
+					SiteScraperUtility.Scrape(
+						urlPathTuple.Item1,
+						siteDirectory);
+				}
+				catch (UriFormatException e)
+				{
+					System.Console.Error.WriteLine(e.Message);
+					Environment.Exit(-1);
+				}
 			}
 		}
 
 		string[] m_args;
-		ConcurrentQueue<string> m_urlQueue;
+		ConcurrentQueue<Tuple<string, string>> m_urlQueue;
 		readonly string m_usage;
 	}
 }
--- SiteScraper/SiteScraper/SiteScraperUtility.cs
+++ SiteScraper/SiteScraper/SiteScraperUtility.cs
@@ -20,60 +20,195 @@
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 using System;
 using System.Collections;
-using System.Net;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using HtmlAgilityPack;
 
 namespace SiteScraper
 {
 	public class SiteScraperUtility
 	{
+		// Example Inputs
+		// url: http://www.google.com/foo/bar
+		// path: /home/username/Desktop/google/
 		public static void Scrape(string url, string path)
 		{
-			System.Console.WriteLine("Scraping url:{0}\n\tto Path:{1}", url, path);
-			string data = GetUrl(url);
-			GetLinks(data);
-		}
-
-		static string GetUrl(string url)
-		{
-			using(WebClient client = new WebClient())
+			try
 			{
-				return client.DownloadString(url);
+				// Full path of saved file should be path+url(after top level domain)
+				//System.Console.WriteLine("Scraping url:{0}\n\tto Path:{1}", url, path);
+
+				// Check if this portion has already completed
+				Uri uri = new Uri(url);
+				int depth = 0;
+				string dataPath = null;
+				if (uri.AbsolutePath == "/")
+				{
+					dataPath = Path.Combine(path, c_indexFileName);
+					if (File.Exists(dataPath))
+						return;
+					else
+					{
+						GetUrl((new Uri(uri, c_indexFileName)).ToString(), dataPath);
+					}
+				}
+				else
+				{
+					string[] directory = uri.AbsolutePath.Split('/').Where(x => x != "").ToArray();
+					depth = directory.Length;
+					dataPath = dataPath ?? "";
+					foreach (string dir in directory.Reverse().Skip(1).Reverse())
+					{
+						dataPath = Path.Combine(dataPath, dir);
+						string currentLevel = (new Uri(Path.Combine(path, dataPath))).AbsolutePath;
+						if (!FileOrDirectoryExists(currentLevel))
+						{
+							System.Console.WriteLine("Creating Dir:@{1}:{0}", currentLevel, new System.Diagnostics.StackTrace(true).GetFrame(0).GetFileLineNumber());
+							Directory.CreateDirectory(currentLevel);
+						}
+					}
+					// If the file doesn't have an extension append html
+					string filename = directory.Last();
+					IfNecessaryAppendHtml(ref filename);
+					dataPath = Path.Combine(path, Path.Combine(dataPath, filename));
+					if (File.Exists(dataPath))
+					{
+						System.Console.Error.WriteLine("{0} already exists", dataPath);
+						return;
+					}
+					else
+					{
+						if (filename.Split('.').Where(x => x != "").Last().StartsWith("htm"))
+							depth = directory.Length - 1;
+						GetUrl(url, dataPath);
+					}
+				}
+
+				List<string> resources = GetLinks(dataPath, depth);
+
+				foreach (string resource in resources)
+				{
+					if (resource.First() == '/')
+					{
+						string nextUrl = String.Format("{0}{1}{2}{3}", uri.Scheme, Uri.SchemeDelimiter, uri.Authority, resource);
+						//System.Console.WriteLine("nextUrl:{0}", nextUrl);
+						SiteScraperUtility.Scrape(nextUrl, path);
+					}
+					else
+					{
+						//File that is an outside resource
+					}
+				}
+			}
+			catch(WebException e)
+			{
+				Console.WriteLine("WebExceptionMessage :" + e.Message);
+				if(e.Status == WebExceptionStatus.ProtocolError) {
+					Console.WriteLine("Status Code : {0}", ((HttpWebResponse)e.Response).StatusCode);
+					Console.WriteLine("Status Description : {0}", ((HttpWebResponse)e.Response).StatusDescription);
+				}
 			}
 		}
 
-		static string[] GetLinks(string urlData)
+		static void GetUrl(string url, string path)
+		{
+			//System.Console.Write("Downloading {0}", new Uri(url));
+			//System.Console.WriteLine(" to {0}", new Uri(path).AbsolutePath);
+			using(WebClient client = new WebClient())
+			{
+				client.DownloadFile(url, path);
+			}
+		}
+
+		static List<string> GetLinks(string urlData, int depth)
 		{
 			HtmlDocument doc = new HtmlDocument();
-
-			doc.LoadHtml(urlData);
+			List<string> resources = new List<string>();
+			doc.Load(urlData);
 			if (doc.ParseErrors == null || doc.ParseErrors.Count() == 0)
 			{
 				if (doc.DocumentNode != null)
 				{
-					foreach (HtmlNode link in doc.DocumentNode.SelectNodes("//link[@href]"))
+					foreach (HtmlNode link in doc.DocumentNode.SelectNodes("//link[@href]").EmptyOrNotNull())
 					{
 						HtmlAttribute att = link.Attributes["href"];
-						System.Console.WriteLine(att.Value);
+						resources.Add(att.Value);
+						string attributeValue = att.Value;
+						IfNecessaryAppendHtml(ref attributeValue);
+						if (attributeValue.ToCharArray().First() == '/')
+						{
+							string prependDepth = ".";
+							for (int i = 0; i < depth; ++i)
+							{
+								prependDepth += "/..";
+							}
+							attributeValue = string.Format("{1}{0}", attributeValue, prependDepth);
+						}
+						att.Value = attributeValue;
+						link.Attributes["href"] = att;
 					}
-					System.Console.WriteLine();
-					foreach (HtmlNode imgLink in doc.DocumentNode.SelectNodes("//img[@src]"))
+					foreach (HtmlNode imgLink in doc.DocumentNode.SelectNodes("//img[@src]").EmptyOrNotNull())
 					{
 						HtmlAttribute att = imgLink.Attributes["src"];
-						System.Console.WriteLine(att.Value);
+						resources.Add(att.Value);
+						string attributeValue = att.Value;
+						IfNecessaryAppendHtml(ref attributeValue);
+						if (attributeValue.ToCharArray().First() == '/')
+						{
+							string prependDepth = ".";
+							for (int i = 0; i < depth; ++i)
+							{
+								prependDepth += "/..";
+							}
+							attributeValue = string.Format("{1}{0}", attributeValue, prependDepth);
+						}
+						att.Value = attributeValue;
+						imgLink.Attributes["src"] = att;
 					}
-					System.Console.WriteLine();
-					foreach (HtmlNode hyperLink in doc.DocumentNode.SelectNodes("//a[@href]"))
+					foreach (HtmlNode hyperLink in doc.DocumentNode.SelectNodes("//a[@href]").EmptyOrNotNull())
 					{
 						HtmlAttribute att = hyperLink.Attributes["href"];
-						System.Console.WriteLine(att.Value);
+						resources.Add(att.Value);
+						string attributeValue = att.Value;
+						IfNecessaryAppendHtml(ref attributeValue);
+						if (attributeValue.ToCharArray().First() == '/')
+						{
+							string prependDepth = ".";
+							for (int i = 0; i < depth; ++i)
+							{
+								prependDepth += "/..";
+							}
+							attributeValue = string.Format("{1}{0}", attributeValue, prependDepth);
+						}
+						att.Value = attributeValue;
+						hyperLink.Attributes["href"] = att;
 					}
+					doc.Save(urlData);
 				}
 			}
-
-			return null;
+			return resources;
 		}
+
+		public static bool FileOrDirectoryExists(string name)
+		{
+			return (Directory.Exists(name) || File.Exists(name));
+		}
+
+		static bool HasAFileExtension(string filename)
+		{
+			return filename.Split('.').Last() != filename && filename.Split('.').Last().Length <= 4;
+		}
+
+		static void IfNecessaryAppendHtml(ref string filename)
+		{
+			if (!HasAFileExtension(filename))
+				filename = String.Format("{0}{1}", filename, c_noExtensionFile);
+		}
+
+		public const string c_indexFileName = "index.html";
+		public const string c_noExtensionFile = ".html";
 	}
 }
 
